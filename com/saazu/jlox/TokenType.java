8fc1aa76f7a82d7d0de57ef8aae37d7b7c7a5c61
Alfred Delle <alfred.delle@yale.edu>
2021-07-24 16:43:27 +0000

    TokenType emum, listing all the tokens in the jlox language, Token Class, main class for the JLox language project, JLox.java

---
+++ com/saazu/jlox/TokenType.java
@@ -0,0 +1,17 @@
+package com.saazu.jlox;
+
+enum TokenType {
+  // single tokens
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // character tokens with double equivalents?
+  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+  // literals
+  IDENTIFIER, STRING, NUMBER,
+
+  // KEYWORDS
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+}
